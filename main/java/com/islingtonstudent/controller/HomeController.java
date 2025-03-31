32501612c31dc43e1ae8742528cb6ff846454f74
Soniya Sapkota <soniyasapkota@Soniyas-MacBook-Air.local>
2025-03-31 08:58:19 +0545

    project Initialization

---
+++ main/java/com/islingtonstudent/controller/HomeController.java
@@ -0,0 +1,29 @@
+package com.islingtonstudent.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author 23050320 Soniya Sapkota 
+ */
+/**
+ * Servlet implementation class HomeController
+ */
+
+@WebServlet(asyncSupported = true, urlPatterns = {"/home","/"})
+public class HomeController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+    @Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
+    	//send redirect || requestdispatcher
+    	// load jsp design page
+    	req.getRequestDispatcher("WEB-INF/pages/home.jsp").forward(req, response);
+		
+	} 
+
+}
---
+++ main/java/com/islingtonstudent/util/ValidationUtil.java
@@ -0,0 +1,20 @@
+package com.islingtonstudent.util;
+
+public class ValidationUtil {
+	
+	public boolean isAlphabetsOnly(String data) {
+		//return Regular expression (RegEx)
+	}
+	
+	public boolean isCorrectModule(String moduleName) {
+		if(isAlphabetsOnly(moduleName)) {
+			
+		}
+	}
+	
+	public boolean isnumbersOnly(String data) {
+		//return Regular expression (RegEx)
+	}
+	
+ 
+}
